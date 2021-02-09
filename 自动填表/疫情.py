efa7af1a8d6b7ec0910337c5ce251cb5c1b43977
WCX1024979076 <61685956+WCX1024979076@users.noreply.github.com>
2021-02-09 22:57:23 +0800

    Update 疫情.py
    修改路径信息，便于用户使用

--- 自动填表/疫情.py
+++ 自动填表/疫情.py
@@ -2,12 +2,15 @@ import requests
 import json,urllib,time,http
 import time 
 import math
-filename = '/home/UPC-nCoV-submit/cookies.txt'
-cookie = http.cookiejar.LWPCookieJar(filename)
+#使用前请修改以下路径，例如 C:/cookie.txt
+cookie_file='/home/UPC-nCoV-submit/cookies.txt' #保存cookie文件
+user_file="/home/UPC-nCoV-submit/用户信息.txt"   #用户信息文件
+log_file="/home/UPC-nCoV-submit/result.txt"     #日志文件
+cookie = http.cookiejar.LWPCookieJar(cookie_file)
 handler = urllib.request.HTTPCookieProcessor(cookie)
 
 opener = urllib.request.build_opener(handler)
-with open("/home/UPC-nCoV-submit/用户信息.txt",'r',encoding='utf8') as load_f:
+with open(user_file,'r',encoding='utf8') as load_f:
     userinfo = json.load(load_f)
 params = urllib.parse.urlencode(userinfo)
 with opener.open('https://app.upc.edu.cn/uc/wap/login/check', data=bytes(params, 'utf-8')) as resp:
@@ -17,7 +20,7 @@ cookie.save(ignore_discard=True, ignore_expires=True)
 headers1 = ('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',)
 headers2=('X-Requested-With', 'XMLHttpRequest')
 cookie = http.cookiejar.LWPCookieJar()
-cookie.load(filename, ignore_discard=True, ignore_expires=True)
+cookie.load(cookie_file, ignore_discard=True, ignore_expires=True)
 handler = urllib.request.HTTPCookieProcessor(cookie)
 opener = urllib.request.build_opener(handler)
 opener.addheaders = [headers1,headers2]
@@ -27,7 +30,7 @@ with opener.open('https://app.upc.edu.cn/ncov/wap/default/index') as resp:
 print(result)
 result["date"]=time.strftime("%Y%m%d", time.localtime())
 print(result)
-with open("/home/UPC-nCoV-submit/result.txt","a",encoding='utf8') as p :     
+with open(log_file,"a",encoding='utf8') as p :     
     jsObj = json.dumps(result)
     p.write("\n--------------------------\n")
     p.write(jsObj)
